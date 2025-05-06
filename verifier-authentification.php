b9ec6838087497589471b849f77db5586a6cab4e
Louishince <hincelouis@gmail.com>
2025-05-06 15:58:51 -0400

    auth

--- verifier-authentification.php
+++ verifier-authentification.php
@@ -2,38 +2,52 @@
 require('include/Configuration.inc');
 
 if (!empty($_POST)) {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $codeOk = false;
+    $mdpOk = false;
+    $actifOk = false;
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
         $code = htmlspecialchars($_POST['code'], ENT_QUOTES);
         $mdp = htmlspecialchars($_POST['motdepasse'], ENT_QUOTES);
 
         $requete = "SELECT code, motdepasse, actif, prenom, nomfamille FROM usagers WHERE code = '$code'";
-        try {
+        try
+        {
             $resultat = $pdo->query($requete);
-            $utilisateur = $resultat->fetch(PDO::FETCH_ASSOC);
+            $usager = $resultat->fetch(PDO::FETCH_ASSOC);
 
-            if ($utilisateur) {
-                if ($code === $utilisateur['code']) {
-                    if (password_verify($mdp, $utilisateur['motdepasse'])) {
-                        if ($utilisateur['actif']) {
-                            $_SESSION['message_operation'] = "Vous avez été connecté avec succès " . $utilisateur['prenom'] . " " . $utilisateur['nomfamille'] . "!";
-                            $_SESSION['est_authentifie'] = true;
-                            // Effectuer ici la redirection vers la page souhaitée après la connexion
-                            // header('Location: page_apres_connexion.php');
-                            exit(); // Assurez-vous de terminer le script après la redirection
-                        } else {
-                            $_SESSION['message_operation'] = "Votre compte est inactif.";
-                        }
-                    } else {
-                        $_SESSION['message_operation'] = "Mot de passe incorrect.";
-                    }
+            if ($usager)
+            {
+                if ($code === $usager['code'])
+                {
+                    $codeOk = true;
+                }
+
+                if (password_verify($mdp, $usager['motdepasse']))
+                {
+                    $mdpOk = true;
+                }
+
+                if ($usager['actif'])
+                {
+                    $actifOk = true;
+                }
+
+                if ($codeOk && $mdpOk && $actifOk)
+                {
+                    $_SESSION['message_operation'] = "Vous avez été connecté avec succès " . $usager['prenom'] . " " . $usager['nomfamille'] . "!";
+                    $_SESSION['est_authentifie'] = true;
                 } else {
-                    $_SESSION['message_operation'] = "Code d'utilisateur incorrect.";
+                    $_SESSION['message_operation'] = "Oups, une erreur s'est produite lors de l'authentification";
                 }
             } else {
-                $_SESSION['message_operation'] = "Aucun utilisateur trouvé avec ce code.";
+                $_SESSION['message_operation'] = "Oups, une erreur s'est produite lors de l'authentification";
             }
-        } catch (PDOException $e) {
-            echo "<p class='message-erreur'>Nous sommes désolés, une erreur s'est produite lors de la vérification.</p>";
+        }
+        catch (PDOException $e)
+        {
+            echo "<p class='message-erreur'>Nous sommes désolés, les équipes ne peuvent pas être affichées.</p>";
             error_log("Erreur PDO: " . $e->getMessage());
         }
     }
