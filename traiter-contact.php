6136aa0b4b80169e703c59a6e5a36ce9ea6b7783
Louishince <hincelouis@gmail.com>
2025-05-06 09:53:22 -0400

    traiter-contact ajusté

--- traiter-contact.php
+++ traiter-contact.php
@@ -5,6 +5,7 @@ if (!empty($_POST)) {
     // traiter le formulaire
     // effectuer ensuite une redirection vers une autre page
 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nom = htmlspecialchars($_POST['nom'], ENT_QUOTES);
     $message = htmlspecialchars($_POST['message'], ENT_QUOTES);
     $email = htmlspecialchars($_POST['email'], ENT_QUOTES);
@@ -22,19 +23,20 @@ if (!empty($_POST)) {
     $stmt->bindParam(':sujet', $sujet);
 
     if ($stmt->execute()) {
-        session_start();
         $_SESSION['operation_reussie'] = true;
         $_SESSION['message_operation'] = "Votre message a été envoyé avec succès !";
     } else {
-        session_start();
         $_SESSION['operation_reussie'] = false;
         $_SESSION['message_operation'] = "Oups, une erreur s'est produite lors de l'envoi du message.";
         // Vous pouvez afficher l'erreur SQL pour le débogage si nécessaire
         // error_log("Erreur SQL: " . $stmt->errorInfo()[2]);
     }
     $stmt->closeCursor();
+    }
+
     header('Location: index.php');
-    exit(); // Assurez-vous d'arrêter l'exécution du script après la redirection
+    exit; // Assurez-vous d'arrêter l'exécution du script après la redirection
+
 } else {
     // réagir si l'appel ne provient pas du formulaire
     // par exemple, ici, on redirige vers la page d'accueil sans avertissement
