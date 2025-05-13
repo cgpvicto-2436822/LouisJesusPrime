fb9f412003c0783da87cbe421a2d772c4800b47e
Louishince <hincelouis@gmail.com>
2025-05-13 14:03:54 -0400

    ex18 v2 + fav icon

--- formulaire-page-accueil.php
+++ formulaire-page-accueil.php
@@ -2,10 +2,21 @@
 require ('include/Configuration.inc');
 require ('entete.inc');
 
+echo '<script src="https://cdn.tiny.cloud/1/5m45apnq1crleiocbdn7ki3zjk6k6zylhmzp9dyhltdif5x1/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>';
+echo '<script>
+tinymce.init({
+    selector:"textarea.tinymce",
+    menubar: false,
+    plugins: "link, image, lists, code, table",
+    toolbar: "bold italic | link image | bullist numlist | table | pastetext removeformat | code"
+})
+</script>';
+
+
 if ($_SESSION['est_authentifie'] == true) {
     echo '<form id="mon-formulaire" method="post" action="enregistrer-page-accueil.php">';
     echo '    <label for="texte">* Texte de la page du menu</label>';
-    echo '    <textarea id="texte" name="texte">';
+    echo '    <textarea class="tinymce" id="texte" name="texte"></textarea>';
     echo '    <button type="submit">Envoyer</button>';
     echo '</form>';
 }
